4ce1aff8517931c00653380d80b723f9261635aa
Nicholas <aganirabn@gmail.com>
2022-09-21 16:41:45 -0700

    worked on homework03 a bit

--- school/homework03/homework03p2.cpp
+++ school/homework03/homework03p2.cpp
@@ -7,16 +7,29 @@ int main() {
       day of (adjective3) (noun1), he/she will never be seen in a(n) (noun2)
     */
 
-    std::string adjective1; 
-    std::string adjective2;   
-    std::string adjective3;
-    std::string name;
-    std::string noun1;
-    std::string noun2;
+  std::string adjective1; 
+  std::string adjective2;   
+  std::string verbalNoun;
+  std::string name;
+  std::string noun1;
+  std::string noun2;
 
-    std::cout << "give me three adjectives, two nouns, and a name";
-    
-    getline(std::cin, adjective1)
-    
+  std::cout << "give me an adjective" << std::endl;
+  getline(std::cin, adjective1);
+  std::cout << "give me another adjective" << std::endl;
+  getline(std::cin, adjective2);
+  std::cout << "give me a verbal noun" << std::endl;
+  getline(std::cin, verbalNoun);
+  std::cout << "give me a noun" << std::endl;
+  getline(std::cin, noun1);
+  std::cout << "give me another noun" << std::endl;
+  getline(std::cin, noun2);
+  std::cout << "give me a name" << std::endl;
+  getline(std::cin, name);
 
+  std::cout << "While the " << adjective1 << " " << name << " has never been known to shy away from a(n) " << adjective2 << " day of " 
+  << verbalNoun << " " << noun1 << ", he/she will never be seen in a(n) " << noun2 << std::endl;
+
+
+  return 0;
 }
---
+++ school/homework03/homework03p3.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+int main() {
+
+    std::string name;
+    std::string adjective;
+    std::string noun;
+    std::string adjective1; 
+    std::string adjective2;   
+    std::string verbalNoun;
+    std::string name1;
+    std::string noun1;
+    std::string noun2;
+    
+    
+
+
+
+
+    std::cout << "Give me a name"
+    << std:: endl; 
+    getline(std::cin, name);
+    int numberOfCharsName = name.size();
+    std::cout << "Give me an adjective"
+    << std:: endl;
+    getline(std::cin, adjective);
+    int numberOfCharsAdjective = adjective.size();
+    std::cout << "Give me a noun"
+    << std:: endl;
+    getline(std::cin, noun);
+    int numberOfCharsNoun = noun.size();
+    std::cout << "Please excuse " << name << " who is far too " << adjective << " to attend " << noun << " class" << std::endl;
+
+    int numberOfCharsMadlib1 = numberOfCharsName + numberOfCharsAdjective + numberOfCharsNoun; 
+    std::cout << "The number of chars in the first madlib is " << numberOfCharsMadlib1 << std::endl;
+
+
+    std::cout << "lets play again, give me an adjective" << std::endl;
+    getline(std::cin, adjective1);
+    int numberOfCharsAdjective1 = adjective1.size();
+    std::cout << "give me another adjective" << std::endl;
+    getline(std::cin, adjective2);
+    int numberOfCharsAdjective2 = adjective2.size();
+    std::cout << "give me a verbal noun" << std::endl;
+    getline(std::cin, verbalNoun);
+    int numberOfCharsVerbalNoun = verbalNoun.size();
+    std::cout << "give me a noun" << std::endl;
+    getline(std::cin, noun1);
+    int numberOfCharsNoun1 = noun1.size();
+    std::cout << "give me another noun" << std::endl;
+    getline(std::cin, noun2);
+    int numberOfCharsNoun2 = noun2.size();
+    std::cout << "give me a name" << std::endl;
+    getline(std::cin, name1);
+    int numberOfCharsName1 = name1.size();
+
+    std::cout << "While the " << adjective1 << " " << name1 << " has never been known to shy away from a(n) " << adjective2 << " day of " 
+    << verbalNoun << " " << noun1 << ", he/she will never be seen in a(n) " << noun2 << std::endl;
+
+    int numberOfCharsMadLib2 = numberOfCharsAdjective1 + numberOfCharsAdjective2 + numberOfCharsVerbalNoun + numberOfCharsNoun1 + numberOfCharsNoun2 + 
+    numberOfCharsName1;
+
+    std::cout << "The number of chars in the second madlib is " << numberOfCharsMadLib2 << std::endl;
+
+
+
+
+
+
+        
+
+    return 0;
+
+
+}
