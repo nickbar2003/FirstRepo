240aeb90adb1323077a9f9c00e3e918623192893
Nicholas <aganirabn@gmail.com>
2022-09-21 11:03:05 -0700

    homework03

---
+++ school/homework03/homework03p2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    /* 
+     While the (adjective1) (Name) has never been known to shy away from a (adjective2)
+      day of (adjective3) (noun1), he/she will never be seen in a(n) (noun2)
+    */
+
+    std::string adjective1; 
+    std::string adjective2;   
+    std::string adjective3;
+    std::string name;
+    std::string noun1;
+    std::string noun2;
+
+    std::cout << "give me three adjectives, two nouns, and a name";
+    
+    getline(std::cin, adjective1)
+    
+
+}
