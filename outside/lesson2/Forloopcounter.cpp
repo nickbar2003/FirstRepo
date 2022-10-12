d896e247a7c2f2b1d5c875b5246d342e36e974e2
Barinaga <nbarinaga@BC003-33826.gonzaga.edu>
2022-10-12 11:00:49 -0700

    just changed a name

---
+++ outside/lesson2/Forloopcounter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main() {
+
+    for(int count = 100; count < 151; count++) {
+        std::cout << "Count is: " << count<< "\n";
+        
+    }
+
+   
+
+    return 0;
+}
