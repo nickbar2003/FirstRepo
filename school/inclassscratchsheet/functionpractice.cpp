ed3cc3637de2c724f9c9e3153614fe19307fa798
Barinaga <nbarinaga@BC003-33826.gonzaga.edu>
2022-10-28 11:49:27 -0700

    function practice

--- school/inclassscratchsheet/functionpractice.cpp
+++ school/inclassscratchsheet/functionpractice.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
 
 
-int simplePrint() {
-    std::cout << "I am simple print\n";\
-    int myAge = 19;
-return myAge;
-}
+double mulDoubles(double, double); 
+double divInts(int, int);    
+bool isPositive(int);
+
 
 int main() {
-    std::cout << "Main is here" << std::endl;
-    
-    int resultValue = simplePrint();
-   
-    std:: cout << resultValue << "\n"; 
-    std::cout << "End of Main" << std::endl;
+
+    double muldoubleResult = mulDoubles(6.4, 4.5);
+    double divIntsResult = divInts(80, 6);
+    bool isPositiveResult = isPositive(60);
 
 
 
-
-
-
-
+    std::cout << "Doubles mul: " << muldoubleResult << std::endl;
+    std::cout << "Ints div: " << divIntsResult << std::endl;
+    std::cout << "bool res: " << isPositiveResult << std::endl; 
 
 
     return 0;
+}
+
+
+double mulDoubles(double firstDouble, double secondDouble) {
+
+    double result;
+
+    result = firstDouble * secondDouble;
+    
+    return result;
+}
+
+double divInts(int num, int den) {
+
+    double result;
+    double numDoulbe = (double)num;
+    double denDouble = (double)den;
+
+    result = numDoulbe / denDouble;
+
+
+
+    return result;
+}
+
+bool isPositive(int valueToCheck) {
+
+    bool result = false;
+
+    if(valueToCheck > 0) {
+        result = true;
+    } else {
+        result = false;
+    }
+
+
+
+
+    return result;
 }
