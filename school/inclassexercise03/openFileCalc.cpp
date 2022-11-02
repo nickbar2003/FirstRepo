ec8b9046965fbb2c415d3805b869376f09ff9083
Nicholas <aganirabn@gmail.com>
2022-11-02 11:02:38 -0700

    just updating

--- school/inclassexercise03/openFileCalc.cpp
+++ school/inclassexercise03/openFileCalc.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 #include <fstream>
 
+std::string dataCollect();
+
 
 int main() {
+    std::string intList;
+    intList = dataCollect();
 
-std::ifstream dataFile;
-std::string intList = "";
+    while(std::cin >> intList) {
 
-dataFile.open("dataFile.dat");
+        int numbersRead = 0;
+        stoi(inlist)
 
-if(dataFile.is_open()) {
-   
-while(getline(dataFile, intList)) {
-    
-    std::cout << intList << "\n";
+
+    numbersRead++;
     }
-} else {
+    
+    
+
+
+
+}
+
+
+
+
+
+std::string dataCollect() {
+
+    std::ifstream dataFile;
+    std::string intListAsString = "";
+    std:: string result = "";
+
+    dataFile.open("dataFile.dat");
+
+    if(dataFile.is_open()) {
+   
+        while(getline(dataFile, intListAsString)) {}
+
+    } else {
 
     std::cout << "Could not open file";
 }
 
-while(stoi(intList))
-std::cout << intList;
+result = intListAsString;  
+return result;
 
 
 }
