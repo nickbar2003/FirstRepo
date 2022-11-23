1d570b2e938abb4bbb307b85829d670eca2b35cc
Nicholas <aganirabn@gmail.com>
2022-11-22 16:52:53 -0800

    worked on rosary

--- outside/rosaryProject/headerRos.h
+++ outside/rosaryProject/headerRos.h
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <fstream>
 
 using namespace std::this_thread;
 using namespace std::chrono;
 
-int loopDelay_ms = 100;
-bool stillPraying = true;
+const int LOOP_DELAY_MS = 40;
+const int DECADE = 10;
+
+std::string readPrayers(std::string);
+void printPrayers(std::string);
+void joyfulRos();
+
--- outside/rosaryProject/main.cpp
+++ outside/rosaryProject/main.cpp
@@ -2,37 +2,61 @@
 
 
 int main() {
-
-    int arraySize = 10;
-    int test[arraySize];
-
-
-
-    for(int count = 0; count <arraySize; count++){
-        test[count] = 101;
-
-    }
-
-    while(stillPraying) {
-        sleep_for(milliseconds(loopDelay_ms));
-        for(int count = 0; count < arraySize; count++){
-            
-            
-            std::cout << test[count] << " ";
-            
-        }
-        std::cout << "\n";
-        stillPraying = 0;
-
-
-
-    }
+    
+    joyfulRos();
+    
     
 
+}
 
+// Takes in file path and returns contents 
+std::string readPrayers(std::string textFile) {
+    std::string buffer;
+    std::string curLine;
+    std::fstream prayerFile;
+
+    prayerFile.open(textFile);
+
+    while (std::getline(prayerFile, curLine)) {
+        buffer += curLine + "\n";
+    }
+    prayerFile.close();
+    return buffer;
+}
+
+void printPrayers(std::string prayerName) {
+
+
+    
+    for(int index = 0; index < prayerName.size(); index++) {
+        std::cout.flush();
+        sleep_for(milliseconds(LOOP_DELAY_MS));
+        std::cout << prayerName[index];
+    }
+
+}
+
+void joyfulRos() {
+    std::string aCreed = readPrayers("aCreed.txt");
+    std::string hailMary = readPrayers("hailMary.txt");
+    std::string dox = readPrayers("doxology.txt");
+    std::string ohMyJesus = readPrayers("ohMyJesus.txt");
+    std::string ourFather = readPrayers("ourFather.txt");
+
+    std::cout << "The Joyful \n\n";
+    printPrayers(aCreed);
+    std::cout << "For increase in Faith\n\n";
+    printPrayers(hailMary);
+    std::cout << "For increase in Hope\n\n";
+    printPrayers(hailMary);
+    std::cout << "For increase in Love\n\n";
+    printPrayers(hailMary);
+    printPrayers(dox);
+    std::cout << "The first joyful mystery: The Annunciation\n\n";
+    for(int count = 0; count < DECADE; count++) {
+        printPrayers(hailMary);
+    }
 
 
 
 }
-
-
