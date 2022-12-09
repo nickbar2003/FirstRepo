d99afde677e6a8f4b21267b98e16030f986dd96b
Nicholas <aganirabn@gmail.com>
2022-12-08 16:08:20 -0800

    update

--- school/homeWork/homework07/header.h
+++ school/homeWork/homework07/header.h
@@ -5,24 +5,30 @@
 #include <vector>
 
 void generateRandomValues(std::vector<int> &intsVector, int);
-void printVectors(std::vector<int> &intsVector, std::vector<int> &oddNumbers,
-                  std::vector<int> &evenNumbers,
-                  std::vector<int> &primeNumbers);
+void printVectors(std::vector<int> &);
 void numberProccesing(std::vector<int> &intsVector,
                       std::vector<int> &oddNumbers,
                       std::vector<int> &evenNumbers,
                       std::vector<int> &primeNumbers);
 void writeVectorToFile(std::ofstream &, std::vector<int> &);
+void showVectorStats(std::string, std::vector<int> &);
+
+int calcVectorSum(std::vector<int> &);
+int calcVectorMean(std::vector<int> &);
+
 bool isPrime(int);
 bool isOdd(int);
 bool isEven(int);
-std::string evenOrOdd(int);
 
 std::ofstream oddsFile;
 std::ofstream evensFile;
 std::ofstream primesFile;
 
 std::string userInput;
+std::string oddNumbersTitle = "Odd Numbers ";
+std::string evenNumbersTitle = "Even Numbers";
+std::string primeNumbersTitle = "Prime Numbers";
+std::string allNumbersTitle = "All Numbers ";
 
 int quantityToGenerate;
 const int defaultAmount = 20;
--- school/homeWork/homework07/homework07.cpp
+++ school/homeWork/homework07/homework07.cpp
@@ -1,16 +1,5 @@
 #include "header.h"
 
-void generateRandomValues(std::vector<int> &intsVector, int);
-void printVectors(std::vector<int> &);
-void numberProccesing(std::vector<int> &intsVector,
-                      std::vector<int> &oddNumbers,
-                      std::vector<int> &evenNumbers,
-                      std::vector<int> &primeNumbers);
-bool isPrime(int);
-bool isOdd(int);
-bool isEven(int);
-std::string evenOrOdd(int);
-
 int main() {
 
   std::vector<int> intsVector;
@@ -30,18 +19,28 @@ int main() {
   }
   generateRandomValues(intsVector, quantityToGenerate);
   numberProccesing(intsVector, oddNumbers, evenNumbers, primeNumbers);
-  std::cout << "\n\nGenerating 20 random intergers\n";
+  std::cout << "\n\nGenerating " << quantityToGenerate
+            << " random intergers\n\n";
+  std::cout << allNumbersTitle << ": ";
   printVectors(intsVector);
   std::cout << "\n";
-  std::cout << "Odd Numbers: ";
+  std::cout << oddNumbersTitle << ": ";
   printVectors(oddNumbers);
   std::cout << "\n";
-  std::cout << "Even Numbers: ";
+  std::cout << evenNumbersTitle << ": ";
   printVectors(evenNumbers);
   std::cout << "\n";
-  std::cout << "Prime Numbers: ";
+  std::cout << primeNumbersTitle << ": ";
   printVectors(primeNumbers);
   std::cout << "\n";
+  writeVectorToFile(oddsFile, oddNumbers);
+  writeVectorToFile(evensFile, evenNumbers);
+  writeVectorToFile(primesFile, primeNumbers);
+
+  showVectorStats(allNumbersTitle, intsVector);
+  showVectorStats(oddNumbersTitle, oddNumbers);
+  showVectorStats(evenNumbersTitle, evenNumbers);
+  showVectorStats(primeNumbersTitle, primeNumbers);
 }
 
 void generateRandomValues(std::vector<int> &intsVector,
@@ -137,4 +136,33 @@ void writeVectorToFile(std::ofstream &fileToWriteTo,
       fileToWriteTo << vectorToWrite.at(i) << "\n";
     }
   }
+}
+
+int calcVectorSum(std::vector<int> &vectorToSum) {
+  int sum = 0;
+  for (int i = 0; i < vectorToSum.size(); i++) {
+    sum = sum + vectorToSum.at(i);
+  }
+  return sum;
+}
+
+int calcVectorMean(std::vector<int> &vectorToMean) {
+  int sum = 0;
+  int mean = 0;
+  for (int i = 0; i < vectorToMean.size(); i++) {
+    sum = sum + vectorToMean.at(i);
+  }
+  mean = sum / vectorToMean.size();
+  return mean;
+}
+
+void showVectorStats(std::string vectorTitle,
+                     std::vector<int> &vectorReference) {
+  int vectorNumberQuantity = vectorReference.size();
+  int sum = calcVectorSum(vectorReference);
+  int meanTest = calcVectorMean(vectorReference);
+
+  std::cout << "Stats for " << vectorTitle
+            << "  -  num numbers: " << vectorNumberQuantity << " | sum: " << sum
+            << " | mean: " << meanTest << "\n";
 }
