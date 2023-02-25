cd4682281d083d8f64eb1f479815f7b8afaabb3d
Nicholas <aganirabn@gmail.com>
2023-02-25 10:02:35 -0800

    todo website

---
+++ outside/vimPractice/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+  std::string a = "Aaa";
+  std::string b = "Ba";
+
+  if (a > b) {
+    std::cout << a << "\n";
+  } else if (a > b) {
+    std::cout << b << "\n";
+  } else {
+    std::cout << "Neither\n";
+  }
+}
